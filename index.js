12d0792013dcf1602753701f094c89690081448d
easaka <sakakwame2002@gmail.com>
2022-11-21 12:40:12 +0000

    read done

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ admin.initializeApp({
 app.use(express.json())
 app.use(urlencoded({extended: true}))
 
+const db = admin.firestore();
 
 app.post('/create',async(req,res)=> {
 try {
@@ -30,9 +31,32 @@ try {
 } catch (error) {
     res.send(error)
 }
-})
+});
 
-const db = admin.firestore();
+app.get('/read/all', async(req,res)=>{
+try{
+    const usersRef = db.collection("users");
+    const response = await usersRef.get();
+    let responseArr = [];
+    response.forEach(doc => {
+        responseArr.push(doc.data());
+    });
+    res.send(responseArr)
+}catch(error){
+    res.send(error)
+}
+});
+
+app.get('/read/:id',async(req,res)=>{
+    try{
+        const userRef = db.collection("users").doc(req.params.id);
+        const response = await userRef.get();
+
+        res.send(response.data())
+    }catch(error){
+        res.send(error)
+    }
+})
 
 app.listen(3000,()=>{
     console.log("server is running on port 3000");
